f4823e8cccfef0b7faed3f9611bc6d06f9583c3e
Wooobee <m.laux@wooobee.com>
2016-04-11 12:15:37 +0200

    Update helper.rb

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -16,6 +16,7 @@ module SocialShareButton
         html << link_to("","#", {:rel => ["nofollow", rel],
                                   "data-site" => name,
                                   :class => "social-share-button-#{name}",
+                                  :class => "ssb",
                                   :onclick => "return SocialShareButton.share(this);",
                                   :title => h(link_title)}.merge(extra_data).merge(special_data))
       end
